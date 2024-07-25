46f05edffe4b066b58df1df62fa5413141f7ea41
GabrielMolinari16 <gabrielmolinari2003@gmail.com>
2024-07-25 14:26:42 -0300

    Código funcionando

--- Atualiza_estoque.js
+++ Atualiza_estoque.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const XLSX = require('xlsx');
 
-
-
-
      // Função para aguardar o overlay de "aguarde" desaparecer
      async function waitForOverlayToDisappear(page) {
         const overlaySelector = '#progress';    
@@ -44,41 +41,13 @@ const XLSX = require('xlsx');
         return dados;
     }
 
-    function testeTabela() {
-        
-        localArquivo = '../planilha.xlsx';
-        const dados = GetDadosTabela(localArquivo);
-        
-        console.log(dados);
-
-        for( let i = 0; i < dados.length; i++){
-            const item = dados[i];
-            console.log(item.Nivel);
-        }
-    }
-
-
     async function AtualizaEstoque() {
 
         localArquivo = '../planilha.xlsx';
         GetDadosTabela(localArquivo)
         
-        
         const dados = GetDadosTabela(localArquivo);
         console.log(dados);
-        
-        
-        /* dados.forEach((item,Index) => {
-            console.log(`Linha ${rowIndex + 1}:`);
-            headers.forEach((header) => {
-              console.log(`  Coluna ${header}: ${row[header]}`);
-            });
-          });  */
-
-      
-        
-       
-        //const values = fs.readFileSync('../valoresTeste.txt','utf-8').split('\n');
 
         const opcoesInicio = { headless: false, args: ['--start-maximized'] };
          
@@ -109,9 +78,7 @@ const XLSX = require('xlsx');
         await new Promise(resolve => setTimeout(resolve, 2000));
 
         try {
-           /*  const codigo_Estagio_Baixa =  'input[name="codigo_estagio_baixa."]';
-            await page.waitForSelector(codigo_Estagio_Baixa);
-            console.log('Campo código de baixa encontrado.'); */
+          
             const campo_nivel = 'input[name="nivel."]';
             const campo_grupo = 'input[name="grupo."]';
             const campo_subgrupo = 'input[name="subgrupo."]';
@@ -185,70 +152,6 @@ const XLSX = require('xlsx');
             console.error('Erro ao inserir o valor ', error);
         }
 
-       /*  for (let i = 0; i <= 3; i++) {
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            await waitForOverlayToDisappear(page);
-            
-            if (i == 2) {
-
-            const nr_Operadores = 'input[name="nr_operadores."]';
-            await page.waitForSelector(nr_Operadores);
-            console.log('Campo número de operadores encontrado.');
-
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            await page.type(nr_Operadores, '0,0');
-
-            } else {
-                console.log('Pressionando Enter', i);
-                page.keyboard.press('Enter');          
-            }
-        }
-
-        const deposito =  'input[name="codigo_deposito."]';
-        await page.waitForSelector(deposito);
-        console.log('Campo código do depósito encontrado.\n');
- 
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        await page.type(deposito, values[0]); 
-        
-        await waitForOverlayToDisappear(page);
-        page.keyboard.press('F9');    
- 
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
-        const btnEstornarPecas = '[name="cmdEstornarPecas"]';
-        await page.waitForSelector(btnEstornarPecas);
-        await page.click(btnEstornarPecas);
-
-        await waitForOverlayToDisappear(page);
-
-
-        // Repetição para bipar todos os códigos automaticamente: ---------------------------------------
-        
-        for (let i = 1; i < values.length; i++) {
-            console.log('Posição atual: ', i);
-
-            const campo_Leitura = 'input[name="campo_leitura."]';
-            await page.waitForSelector(campo_Leitura);
-            console.log('Campo código do leitura encontrado.\n');
-            
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            await page.type(campo_Leitura, values[i]);    
-
-            await waitForOverlayToDisappear(page);
-            
-            page.keyboard.press('Enter');
-
-            page.on('dialog', async dialog => {
-                console.log(dialog.message()); // Imprime a mensagem do diálogo
-                console.log(values[i]); // Imprime o código da peça 
-        
-                // Aceita o diálogo
-                await dialog.accept();
-            });
-            
-        } */
     }
 
      AtualizaEstoque().catch(console.error);    
-   //testeTabela();
