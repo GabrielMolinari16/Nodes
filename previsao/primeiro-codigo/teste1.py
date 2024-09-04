b557643c39e24ca764fd31a4d408cdf6b8d99751
GabrielMolinari16 <gabrielmolinari.2003@gmail.com>
2024-09-04 08:40:06 -0300

    Organizando as pastas

--- previsao/primeiro-codigo/teste1.py
+++ previsao/primeiro-codigo/teste1.py
@@ -6,11 +6,15 @@ df = pd.read_excel(file_path, sheet_name='Plan2')
 
 # Somar as quantidades de vendas por coleção
 total_por_colecao = df.groupby('COLECAO')['QTDE'].sum().reset_index()
+print(total_por_colecao)
 
 # Renomear a coluna para melhor entendimento
 total_por_colecao.rename(columns={'QTDE': 'TOTAL_VENDIDO'}, inplace=True)
+print(total_por_colecao)
+
 
 # Salvar o resultado em um novo arquivo Excel
 total_por_colecao.to_excel('total_por_colecao.xlsx', index=False)
+
 
 print("Total vendido por coleção salvo em 'total_por_colecao.xlsx'")
